2f396b0eaf807b08796161c912bc81d07c890db8
Cassio Tavares <cassiot@gmail.com>
2025-11-28 05:37:09 +0000

    added more events

--- main.js
+++ main.js
@@ -2,6 +2,29 @@
 
 const changeEvent = new Event('change', { bubbles: true });
 
+const mouseClickEvent = new MouseEvent('click', {
+    bubbles: true, // Event bubbles up the DOM tree
+    cancelable: true,
+    view: window});
+
+const mouseDownEvent = new MouseEvent('mousedown', {
+    bubbles: true, // Event bubbles up the DOM tree
+    cancelable: true,
+    view: window});
+
+const keyUpEvent = new KeyboardEvent('keyup', {
+    key: ' ',
+    keyCode: 32,
+    code: 'Space',
+    bubbles: true,
+    view: window});
+
+const keyDownEventEnter = new KeyboardEvent('keydown', {
+    key: 'Enter',
+    keyCode: 13,
+    cancellable: false,
+    code: 'Enter',
+    bubbles: true});
 
 //Functions
 
